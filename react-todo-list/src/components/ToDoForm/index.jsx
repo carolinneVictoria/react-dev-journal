373f1bc330e6a50c7d8f49e61c68e0a45f4252b0
Carol <carolinevictoria1224@gmail.com>
2026-06-15 10:44:07 -0300

    fix: ajustando bug da submissao do form

--- react-todo-list/src/components/ToDoForm/index.jsx
+++ react-todo-list/src/components/ToDoForm/index.jsx
@@ -2,7 +2,7 @@ import Button from '../Button';
 import { TextInput } from '../TextInput';
 import './to-do-form.style.css'
 
-export function ToDoForm(onSubmit) {
+export function ToDoForm({onSubmit}) {
     return (
         <form className='todo-form' action={onSubmit}>
             <TextInput placeholder="Digite o item que deseja adicionar" />
