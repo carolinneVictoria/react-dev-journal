5fecce6531a0bfd2daed4bdc2435b1980483bf64
Carol <carolinevictoria1224@gmail.com>
2026-06-19 17:34:56 -0300

    ajustando as tipografias de forma reutilizavel

--- react-poup-app/src/App.jsx
+++ react-poup-app/src/App.jsx
@@ -2,16 +2,29 @@ import { Aside } from './components/Aside'
 import { Container } from './components/Container'
 import { Main } from './components/Main'
 import { SearchInput } from './components/SearchInput'
+import { Typography } from './components/Typography'
 
 
 function App() {
 
   return (
     <Container>
+      {/* Sidebar */}
       <Aside />
+
+      {/* Conteudo principal */}
       <Main>
-        <SearchInput />
+        <SearchInput /> {/* Barra de busca */}
+
+        <Typography variant="h1">
+          Olá, Carol
+        </Typography>
+        <Typography variant="body">
+          Veja como estão suas finanças hoje.
+        </Typography>
+
       </Main>
+
     </Container>
   )
 }
