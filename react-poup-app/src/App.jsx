437eb8935beff385529dedc0992d7fffad3767f1
Carol <carolinevictoria1224@gmail.com>
2026-06-22 11:21:45 -0300

    feat: montando e estilizando a progress bar

--- react-poup-app/src/App.jsx
+++ react-poup-app/src/App.jsx
@@ -3,6 +3,7 @@ import Card from './components/Card'
 import { Container } from './components/Container'
 import { DailyBudget } from './components/DailyBudget'
 import { Main } from './components/Main'
+import { SavingStatus } from './components/SavingsStatus'
 import { SearchInput } from './components/SearchInput'
 import { Typography } from './components/Typography'
 
@@ -28,15 +29,27 @@ function App() {
         </div>
 
         <section>
+        {/* primeiro card */}
           <Card>
             <Card.Header>
               Orçamento Diario Disponivel
             </Card.Header>
             <Card.Body>
-              <DailyBudget value={200}/>
+              <DailyBudget value={250}/>
             </Card.Body>
           </Card>
 
+        {/* segundo card */}
+          <Card>
+            <Card.Header>
+              Progresso da Meta Financeira
+            </Card.Header>
+            <Card.Body>
+              <SavingStatus percent={40}/>
+            </Card.Body>
+          </Card>
+
+        {/* terceiro card */}
           <Card>
             <Card.Header>
               Orçamento Diario Disponivel
@@ -46,15 +59,7 @@ function App() {
             </Card.Body>
           </Card>
 
-          <Card>
-            <Card.Header>
-              Orçamento Diario Disponivel
-            </Card.Header>
-            <Card.Body>
-              R$ 200
-            </Card.Body>
-          </Card>
-
+        {/* quarto card */}
           <Card>
             <Card.Header>
               Orçamento Diario Disponivel
