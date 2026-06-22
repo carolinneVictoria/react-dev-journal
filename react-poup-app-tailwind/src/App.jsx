156e0c799be13f6b52f3757fe641ef4280743e20
Carol <carolinevictoria1224@gmail.com>
2026-06-22 15:50:22 -0300

    feat: configuração do ambiente e insercao das variaveis principais

---
+++ react-poup-app-tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./assets/logo.png"
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-neutral-background min-h-screen'>
+        <aside>
+          <img src={logo} alt="poupapp" />
+        </aside>
+      </div>
+    </>
+  )
+}
+
+export default App
