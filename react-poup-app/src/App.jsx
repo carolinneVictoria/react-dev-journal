15688a664b7ee7ce88a9790e8addb66f43207fbe
Carol <carolinevictoria1224@gmail.com>
2026-06-22 11:33:27 -0300

    fix: ajustando layout principal

--- react-poup-app/src/App.jsx
+++ react-poup-app/src/App.jsx
@@ -1,11 +1,14 @@
 import { Aside } from './components/Aside'
-import Card from './components/Card'
 import { Container } from './components/Container'
-import { DailyBudget } from './components/DailyBudget'
 import { Main } from './components/Main'
-import { SavingStatus } from './components/SavingsStatus'
 import { SearchInput } from './components/SearchInput'
 import { Typography } from './components/Typography'
+
+import Card from './components/Card'
+import { DailyBudget } from './components/DailyBudget'
+import { SavingStatus } from './components/SavingsStatus'
+
+import styles from './app.module.css'
 
 
 function App() {
@@ -17,60 +20,61 @@ function App() {
 
       {/* Conteudo principal */}
       <Main>
-        <SearchInput /> {/* Barra de busca */}
+        <div className={styles.container}>
+          <SearchInput /> {/* Barra de busca */}
 
-        <div>
-          <Typography variant="h1">
-            Olá, Carol
-          </Typography>
-          <Typography variant="body">
-            Veja como estão suas finanças hoje.
-          </Typography>
+          <div>
+            <Typography variant="h1">
+              Olá, Carol
+            </Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+
+          <section className={styles.grid}>
+          {/* primeiro card */}
+            <Card>
+              <Card.Header>
+                Orçamento Diario Disponivel
+              </Card.Header>
+              <Card.Body>
+                <DailyBudget value={250}/>
+              </Card.Body>
+            </Card>
+
+          {/* segundo card */}
+            <Card>
+              <Card.Header>
+                Progresso da Meta Financeira
+              </Card.Header>
+              <Card.Body>
+                <SavingStatus percent={40}/>
+              </Card.Body>
+            </Card>
+
+          {/* terceiro card */}
+            <Card>
+              <Card.Header>
+                Orçamento Diario Disponivel
+              </Card.Header>
+              <Card.Body>
+                R$ 200
+              </Card.Body>
+            </Card>
+
+          {/* quarto card */}
+            <Card>
+              <Card.Header>
+                Orçamento Diario Disponivel
+              </Card.Header>
+              <Card.Body>
+                R$ 200
+              </Card.Body>
+            </Card>
+
+          </section>
         </div>
-
-        <section>
-        {/* primeiro card */}
-          <Card>
-            <Card.Header>
-              Orçamento Diario Disponivel
-            </Card.Header>
-            <Card.Body>
-              <DailyBudget value={250}/>
-            </Card.Body>
-          </Card>
-
-        {/* segundo card */}
-          <Card>
-            <Card.Header>
-              Progresso da Meta Financeira
-            </Card.Header>
-            <Card.Body>
-              <SavingStatus percent={40}/>
-            </Card.Body>
-          </Card>
-
-        {/* terceiro card */}
-          <Card>
-            <Card.Header>
-              Orçamento Diario Disponivel
-            </Card.Header>
-            <Card.Body>
-              R$ 200
-            </Card.Body>
-          </Card>
-
-        {/* quarto card */}
-          <Card>
-            <Card.Header>
-              Orçamento Diario Disponivel
-            </Card.Header>
-            <Card.Body>
-              R$ 200
-            </Card.Body>
-          </Card>
-
-        </section>
-
       </Main>
 
     </Container>
