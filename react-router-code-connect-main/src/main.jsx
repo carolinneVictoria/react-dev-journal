a7b8bdc14679f8648dcb782fa99be95e65400f92
Carol <carolinevictoria1224@gmail.com>
2026-06-23 15:29:25 -0300

    feat: montando a proteção do roteamento

--- react-router-code-connect-main/src/main.jsx
+++ react-router-code-connect-main/src/main.jsx
@@ -6,16 +6,26 @@ import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
 import { BlogPost } from './pages/BlogPost/index.jsx'
 import { BrowserRouter, Route, Routes } from "react-router";
+import { ProtectedRoute } from './components/ProtectedRoute/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
         <Routes>
-          <Route path='auth/register' element={<Register />} />
-          <Route path='auth/login' element={<Login />} />
+          <Route path='/auth/register' element={<Register />} />
+          <Route path='/auth/login' element={<Login />} />
 
-          <Route path='/' element={<Feed />} />
-          <Route path='/blog-post' element={<BlogPost />} />
+          <Route path='/' element={
+            <ProtectedRoute>
+              <Feed />
+            </ProtectedRoute>
+            } />
+
+          <Route path='/blog-post' element={
+            <ProtectedRoute>
+              <BlogPost />
+            </ProtectedRoute>
+          } />
       </Routes>
     </BrowserRouter>
   </StrictMode>
