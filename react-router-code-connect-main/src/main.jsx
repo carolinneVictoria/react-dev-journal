51e7b59b256b160456f40a7e1c4a5d059e0093f1
Carol <carolinevictoria1224@gmail.com>
2026-06-23 16:29:56 -0300

    fix: organizando as rotas

--- react-router-code-connect-main/src/main.jsx
+++ react-router-code-connect-main/src/main.jsx
@@ -1,34 +1,10 @@
 import './index.css'
 import { StrictMode } from 'react'
+import { AppRouter } from './router/index.jsx'
 import { createRoot } from 'react-dom/client'
-import { Login } from './pages/Login/index.jsx'
-import { Register } from './pages/Register/index.jsx'
-import { Feed } from './pages/Feed/index.jsx'
-import { BlogPost } from './pages/BlogPost/index.jsx'
-import { BrowserRouter, Route, Routes } from "react-router";
-import { ProtectedRoute } from './components/ProtectedRoute/index.jsx'
-import { Logout } from './pages/Logout/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-        <Routes>
-          <Route path='/auth/register' element={<Register />} />
-          <Route path='/auth/login' element={<Login />} />
-          <Route path='/auth/logout' element={<Logout />} />
-
-          <Route path='/' element={
-            <ProtectedRoute>
-              <Feed />
-            </ProtectedRoute>
-            } />
-
-          <Route path='/blog-post/:slug' element={
-            <ProtectedRoute>
-              <BlogPost />
-            </ProtectedRoute>
-          } />
-      </Routes>
-    </BrowserRouter>
+    <AppRouter />
   </StrictMode>
 )
