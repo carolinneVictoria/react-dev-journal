3c3b83f2b44f68ea91321f3cc26e9f9f44248882
Carol <carolinevictoria1224@gmail.com>
2026-06-23 16:26:03 -0300

    feat: criando segmentos dinamicos da URL

--- react-router-code-connect-main/src/main.jsx
+++ react-router-code-connect-main/src/main.jsx
@@ -23,7 +23,7 @@ createRoot(document.getElementById('root')).render(
             </ProtectedRoute>
             } />
 
-          <Route path='/blog-post' element={
+          <Route path='/blog-post/:slug' element={
             <ProtectedRoute>
               <BlogPost />
             </ProtectedRoute>
