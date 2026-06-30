f57d51f4eb869b21fefe97844854f526a60d74be
Carol <carolinevictoria1224@gmail.com>
2026-06-30 14:05:57 -0300

    ajustes

--- react-pomodoro-todo/frontend/src/components/TitleHeader/index.tsx
+++ react-pomodoro-todo/frontend/src/components/TitleHeader/index.tsx
@@ -3,7 +3,7 @@ import styles from './titleheader.module.css'
 export const TitleHeader = () => {
     return (
         <div className={styles.header}>
-            <h1 className='titulo'>🍅 Pomodoro & Tarefas</h1>
+            <h1 className={styles.titulo}>🍅 Pomodoro & Tarefas</h1>
             <p>Organize seu tempo e seja mais produtivo</p>
         </div>
     )
