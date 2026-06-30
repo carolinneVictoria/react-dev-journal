f19c59b7be6b9b15fc3847ec0a5d43f1598a3031
Carol <carolinevictoria1224@gmail.com>
2026-06-29 22:27:20 -0300

    Update App.tsx

--- react-pomodoro-todo/frontend/src/App.tsx
+++ react-pomodoro-todo/frontend/src/App.tsx
@@ -1,23 +1,57 @@
+import { useState } from "react";
 import Card from "./components/Card"
 import { TitleHeader } from "./components/TitleHeader"
+import Dialog from "./components/Dialog";
+import ToDoForm from "./components/ToDoForm";
 
 function App() {
+  const [showDialog, setShowDialog] = useState(false);
 
+  const openDialog = () => setShowDialog(true);
+  const closeDialog = () => setShowDialog(false);
+
+  const handleFormSubmit: React.ComponentProps<"form">["onSubmit"] = (
+    event
+  ) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const description = formData.get("description");
+
+    console.log(description);
+
+    form.reset();
+    closeDialog();
+  };
+  
   return (
     <>
-    <main>
-      <TitleHeader />
+      <main>
+        <TitleHeader />
 
-      <section className="principal-app">
-        <Card>
-          <Card.Pomodoro/>
-        </Card>
+        <section className="principal-app">
+          <Card>
+            <Card.Pomodoro />
+          </Card>
 
-        <Card>
-          <Card.Todo/>
-        </Card>
-      </section>
-    </main>
+          <Card>
+            <Card.Todo onOpenDialog={openDialog}>
+              <Dialog
+                isOpen={showDialog}
+                onClose={closeDialog}
+              >
+                <ToDoForm
+                  defaultValue=""
+                  onSubmit={handleFormSubmit}
+                />
+              </Dialog>
+            </Card.Todo>
+          </Card>
+
+        </section>
+      </main>
     </>
   )
 }
