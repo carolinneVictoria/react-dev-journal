f82a00b5c21095f104b8b79f4cc620ef98c85616
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-07-20 11:04:03 -0300

    feat: implement authentication logic in Aside component

--- react-router-code-connect-main/src/components/Aside/index.jsx
+++ react-router-code-connect-main/src/components/Aside/index.jsx
@@ -6,11 +6,13 @@ import { IconInfo } from '../icons/IconInfo'
 import { IconLogin } from '../icons/IconLogin'
 import { Button } from '../Button'
 import { Link } from '../Link'
+import { useAuth } from '../../hooks/useAuth'
 
 import styles from './aside.module.css'
 
 export const Aside = () => {
 
+    const { isAuthenticated } = useAuth()
 
     return (<aside className={styles.aside}>
         <nav>
@@ -44,10 +46,15 @@ export const Aside = () => {
                     </AsideLink>
                 </li>
                 <li>
-                    <AsideLink href="auth/logout">
-                        <IconLogin />
-                        Logout
-                    </AsideLink>
+                    {isAuthenticated ? (
+                        <AsideLink href="/auth/logout">
+                            <IconLogin /> Logout
+                        </AsideLink>
+                    ) : (
+                        <AsideLink href="/auth/login">
+                            <IconLogin /> Login
+                        </AsideLink>
+                    )}
                 </li>
             </ul>
         </nav>
