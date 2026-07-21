970587dd908bdcbf0b0e4c02dd6f7df571810b84
Carol <carolinevictoria1224@gmail.com>
2026-07-21 17:22:57 -0300

    feat: replace fetch with http client for blog post retrieval in BlogPost component

--- react-router-code-connect-main/src/pages/BlogPost/index.jsx
+++ react-router-code-connect-main/src/pages/BlogPost/index.jsx
@@ -8,6 +8,8 @@ import ReactMarkdown from 'react-markdown'
 import { useNavigate, useParams } from "react-router"
 import { ModalComment } from "../../components/ModalComment"
 import { useEffect, useState } from "react"
+import { http } from "../../api"
+
 
 export const BlogPost = () => {
 
@@ -16,15 +18,16 @@ export const BlogPost = () => {
     const navigate = useNavigate()
 
     useEffect(() => {
-        fetch(`http://localhost:3000/blog-posts/slug/${slug}`)
+        http.get(`blog-posts/slug/${slug}`)
             .then(response => {
-                if (response.status == 404) {
+                setPost(response.data)
+            })
+            .catch(error => {
+                if (error.status == 404){
                     navigate('/not-found')
                 }
-                return response.json()
             })
-            .then(data => setPost(data))
-    }, [slug])
+    }, [slug, navigate])
 
     if (!post) {
         return null
