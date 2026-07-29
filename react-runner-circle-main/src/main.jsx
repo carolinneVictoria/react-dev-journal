b26e62494464c6f51443c0f06a4dccdc607bfcfa
Carol <carolinevictoria1224@gmail.com>
2026-07-29 17:30:50 -0300

    feat: integrate Apollo Client for GraphQL data management

--- react-runner-circle-main/src/main.jsx
+++ react-runner-circle-main/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const client = new ApolloClient({
+  url: "http://localhost:3001/graphql",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
-)
+);
