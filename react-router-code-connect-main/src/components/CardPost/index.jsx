333436d71436ddd5b7997e2bd97f20b2664016a7
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-07-31 16:12:39 -0300

    commit

--- react-router-code-connect-main/src/components/CardPost/index.jsx
+++ react-router-code-connect-main/src/components/CardPost/index.jsx
@@ -7,6 +7,7 @@ import { useAuth } from "../../hooks/useAuth";
 import { usePostInteractions } from "../../hooks/usePostInteractions";
 import { ThumbsUpButton } from "./ThumbsUpButton";
 
+// inicio do card post
 export const CardPost = ({ post }) => {
   const { isAuthenticated } = useAuth();
   const { likes, comments, handleNewComment, handleLikeButton } =
