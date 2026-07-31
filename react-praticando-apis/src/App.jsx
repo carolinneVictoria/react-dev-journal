365716a4bdd5746e4c50d783ede976fa81ea0b36
Carol <carolinevictoria1224@gmail.com>
2026-07-31 17:37:25 -0300

    feat: add additional min temperature display for the third forecast

--- react-praticando-apis/src/App.jsx
+++ react-praticando-apis/src/App.jsx
@@ -2,13 +2,15 @@ import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 import "./App.css";
 import { useEffect, useState } from "react";
+import ForecastList from "./components/ForecastList";
 
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 function App() {
 
   const [weather, setWeather] = useState(null)
-
+  const [forecast, setForecast] = useState(null)
+  
   useEffect(() => {
     async function fetchWeather() {
       try {
@@ -17,6 +19,8 @@ function App() {
 
         if(data.results) {
           setWeather(data.results)
+          setForecast(data.results.forecast.slice(1, 4));
+          console.log(data.results)
         }
       } catch (error) {
         console.error("Erro ao buscar dados da API", error)
@@ -32,6 +36,7 @@ function App() {
         <>
           <h1>{weather.city}</h1>
           <WeatherCard weather={weather}/>
+          <ForecastList forecasts={forecast} />
         </>
       )}
     </div>
---
+++ react-praticando-apis/src/components/ForecastItem/index.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+const ForecastItem = ({ date, min, max, condition, description }) => {
+  return (
+    <div className="forecast-item">
+      <p className="forecast-day">
+         ({date})
+      </p>
+      <img src={`./icons-weather/${condition}.svg`} alt={description} />
+      <p className="forecast-temp">
+        {min}/{max}°
+      </p>
+    </div>
+  );
+};
+
+export default ForecastItem;
