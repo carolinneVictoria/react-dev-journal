4093c3f356345b59a5dfdaee918a55b45f9a5560
Carol <carolinevictoria1224@gmail.com>
2026-08-02 19:16:00 -0300

    feat: enhance ForecastItem to display weekday alongside date

--- react-praticando-apis/src/components/ForecastItem/index.jsx
+++ react-praticando-apis/src/components/ForecastItem/index.jsx
@@ -1,13 +1,22 @@
 import "./styles.css";
 
+const getWeekday = (dateString) => {
+  const [day, month] = dateString.split("/");
+  const dateObj = new Date(new Date().getFullYear(), month - 1, day);
+  const weekday = Intl.DateTimeFormat("pt-BR", { weekday: "long" }).format(
+    dateObj
+  );
+
+  return weekday.charAt(0).toUpperCase() + weekday.split("-")[0].slice(1);
+};
+
 const ForecastItem = ({ date, min, max, condition, description }) => {
   return (
     <div className="forecast-item">
       <p className="forecast-day">
-         ({date})
+        {getWeekday(date)} ({date})
       </p>
       <img src={`./icons-weather/${condition}.svg`} alt={description} />
-      <span>{description}</span>
       <p className="forecast-temp">
         {min}/{max}°
       </p>
