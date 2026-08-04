112274ed789a599664d3f1a18686f0f8b1e9ac81
Carol <carolinevictoria1224@gmail.com>
2026-08-04 11:39:00 -0300

    feat: update GraphQL queries to include workout field in feed responses

--- react-runner-circle-main/database/graphql/query/feed.js
+++ react-runner-circle-main/database/graphql/query/feed.js
@@ -7,6 +7,20 @@ export const GET_FEED = gql`
             time
             stats
             description
-            }
+            workout
+        }
+    }
+`;
+
+export const GET_FEED_BY_CATEGORY = gql`
+    query GetFeedByCategory($category: String) {
+        allFeeds(filter: { category: $category }) {
+            id
+            user
+            time
+            stats
+            description
+            workout
+        }
     }
 `;
