1bf83cad5b4d3641baaf38164af8f05a84c5f5fa
Carol <carolinevictoria1224@gmail.com>
2026-08-06 14:07:45 -0300

    feat: add GraphQL mutation for deleting feed posts

--- react-runner-circle-main/database/graphql/mutations/feed.js
+++ react-runner-circle-main/database/graphql/mutations/feed.js
@@ -28,3 +28,11 @@ export const ADD_FEED_POST = gql`
     }
 
 `;
+
+export const DELETE_FEED_POST = gql`
+    mutation DeleteFeedPost($id: ID!) {
+        deleteFeed(id: $id) {
+            id
+        }
+    }
+`;
