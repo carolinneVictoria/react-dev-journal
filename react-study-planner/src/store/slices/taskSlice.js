b4e612a27af74d736c6fbf5395ac32eb8ced75da
Carol <carolinevictoria1224@gmail.com>
2026-08-13 10:01:59 -0300

    feat: refactor task slice to include editTask and deleteTask reducers

--- react-study-planner/src/store/slices/taskSlice.js
+++ react-study-planner/src/store/slices/taskSlice.js
@@ -15,12 +15,28 @@ const taskSlice = createSlice({
                 completed: false,
             };
             state.tasks.push(taskWithId);
+        },
+        toggleTaskComplete: (state, action) => {
+            const task = state.task.find(task => task.id === action.payload);
+            if (task) task.completed = !task.completed
+        },
+        editTask: (state, action) => {
+            const {taskId, updateTask } = action.payload;
+            const taskIndex = state.task.findIndex(task => task.id === taskId);
+            if (taskIndex != -1) {
+                state.tasks[taskIndex] = {...state.task[taskIndex], ...updateTask}
+            }
+        },
+        deleteTask: (state, action) => {
+            state.tasks = state.tasks.filter((task) => task.id != action.payload)
         }
     },
-    toggleTaskComplete: (state, action) => {
-        const task = state.task.find(task => task.id === action.payload);
-        if (task) {
-            task.completed = !task.completed
-        }
-    }
-})
+});
+
+export const { addTask, toggleTaskComplete, editTask, deleteTask } = taskSlice.actions;
+
+export const selectTasks = (state) => state.tasks.tasks;
+export const selectPendingTasks = (state) => state.tasks.tasks.filter(task => !task.completed)
+export const selectCompletedTasks = (state) => state.tasks.tasks.filter(task => task.completed)
+
+export default taskSlice.reducer;
