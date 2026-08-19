44655795362321fcfd2596f15b35be9c2490cfb9
Carol <carolinevictoria1224@gmail.com>
2026-08-19 10:00:18 -0300

    fix: correct theme reducer import and add to store configuration

--- react-study-planner/src/store/index.js
+++ react-study-planner/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tasksReducer from './slices/taskSlice'
+import themeReducer from './slices/themeSlice'
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
+    theme: themereducer,
   },
 })
