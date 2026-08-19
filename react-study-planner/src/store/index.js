4836f77e3f33b56236cd5d47c8e4fc8d9b397e1b
Carol <carolinevictoria1224@gmail.com>
2026-08-19 10:47:12 -0300

    fix: add analytics reducer to store configuration

--- react-study-planner/src/store/index.js
+++ react-study-planner/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tasksReducer from './slices/taskSlice'
 import themeReducer from './slices/themeSlice'
+import analyticsReducer from './slices/analyticSlice'
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
     theme: themeReducer,
+    analytics: analyticsReducer,
   },
 })
