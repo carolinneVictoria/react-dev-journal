b06dde19d34795650eb7ac5f6c288864e3739aba
Carol <carolinevictoria1224@gmail.com>
2026-08-24 22:51:47 -0300

    test: add unit tests for mergeClass function to validate class assignment based on completion status

--- react-todo-list/src/components/ToDoItem/mergeClass.spec.js
+++ react-todo-list/src/components/ToDoItem/mergeClass.spec.js
@@ -1,3 +1,17 @@
 describe("./mergeClass.js", () => {
-    
+    test('deve retornar a classe do todo-item quando nao estiver completo', () => {
+        const isItemCompleted = false;
+
+        const styles = mergeClass(isItemCompleted);
+
+        expect(styles).toBe("todo-item");
+    })
+
+    test('deve retornar a classe do todo-item quando estiver completo', () => {
+        const isItemCompleted = true;
+
+        const styles = mergeClass(isItemCompleted);
+
+        expect(styles).toBe("todo-item completed");
+    })
 })
