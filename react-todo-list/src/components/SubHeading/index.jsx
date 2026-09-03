a7193da5e69c6f2bcb1900505b99ba5b6ec1daf3
Carol <carolinevictoria1224@gmail.com>
2026-09-03 14:32:02 -0300

    fix: add early return for empty children in SubHeading component

--- react-todo-list/src/components/SubHeading/index.jsx
+++ react-todo-list/src/components/SubHeading/index.jsx
@@ -1,6 +1,7 @@
 import './sub-heading.style.css'
 
 export function SubHeading ({ children }) {
+    if (!children) return <></>
     return (
         <h2 className='subheading'>
             { children }
