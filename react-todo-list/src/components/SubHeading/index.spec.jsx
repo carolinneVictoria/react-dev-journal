fcb53b87a6c3e9fd6ff9ee7c5ce090463edad6fa
Carol <carolinevictoria1224@gmail.com>
2026-09-03 14:36:35 -0300

    test: reorganize SubHeading component tests into queryBy and getBy groups

--- react-todo-list/src/components/SubHeading/index.spec.jsx
+++ react-todo-list/src/components/SubHeading/index.spec.jsx
@@ -3,13 +3,26 @@ import { render } from '@testing-library/react';
 import { SubHeading } from './index';
 
 describe('SubHeading', () => {
-    test('deveria renderizar o componente corretamente', () => {
-        const {getByText} = render(<SubHeading>Para Estudar</SubHeading>);
-        expect(getByText('Para Estudar')).toBeInTheDocument();
-    });
-
-    test('não deveria renderizar o componente corretamente quando não tem children', () => {
-        const {getByText} = render(<SubHeading>Para Estudar</SubHeading>);
-        expect(getByText('Para Estudar')).toBeInTheDocument();
-    });
+    describe('queryBy', () => {
+        test('deveria renderizar o componente corretamente', () => {
+            const {queryByText} = render(<SubHeading>Para Estudar</SubHeading>);
+            expect(queryByText('Para Estudar')).toBeInTheDocument();
+        });
+    
+        test('não deveria renderizar o componente corretamente quando não tem children', () => {
+            const {queryByText} = render(<SubHeading></SubHeading>);
+            expect(queryByText('Para Estudar')).not.toBeInTheDocument();
+        });
+    })
+    describe('getBy', () => {
+        test('deveria renderizar o componente corretamente', () => {
+            const {getByText} = render(<SubHeading>Para Estudar</SubHeading>);
+            expect(getByText('Para Estudar')).toBeInTheDocument();
+        });
+    
+        test('não deveria renderizar o componente corretamente quando não tem children', () => {
+            const {getByText} = render(<SubHeading></SubHeading>);
+            expect(getByText('Para Estudar')).not.toBeInTheDocument();
+        });
+    })
 });
